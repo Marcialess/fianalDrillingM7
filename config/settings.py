435c7f251e8609436828cf4083c3817ef5acf9bc
Marcial Abad <108768099+Marcialess@users.noreply.github.com>
2023-08-28 20:28:29 -0400

    ultimos cambios

--- config/settings.py
+++ config/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),  #'db_final_orm',
-        "USER": os.getenv("DB_USER"), #"userdjango",
-        "PASSWORD": os.getenv("DB_PASSWORD"),#"userdjango",
-        "HOST": os.getenv("DB_HOST"),#"127.0.0.1",
-        "PORT": os.getenv("DB_PORT"),#"5432",
+        'NAME': os.getenv("DB_NAME"),  
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 import sys
